d7f2134b09fb8dcbea534582c1c89eb5dbbd0ca3
sami3000 <cordobamolinasamira@gmail.com>
2021-11-27 11:36:27 -0300

    practica modelados

---
+++ sami/modelados/Robot.py
@@ -0,0 +1,14 @@
+"""
+    Ejercicio 3:
+        Modelar la clase Robot con las siguientes caracteristicas:
+            - Un robot posee una bateria en la cual almacena su energia.
+              La carga de la bateria de encuentra en el rango de 0 a 100.
+            - Un robot puede:
+                # caminar(metros): le consume 1 unidad cada 10 metros.
+                # cargarBateria(una_carga): le agrega una determinada cantidad
+                  de unidades, pero no puede superar las 100 unidades de la bateria.
+                # disparar(un_objetivo): le consume el 10% de la bateria que posea. 
+
+    Realizar los test necesarios que validen 
+    el comportamiento.         
+"""
